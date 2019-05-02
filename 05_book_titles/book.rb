25b5368f5435d0790f7a456c81a0fb8d8d22e40a
JFAldridge <jfaldridge1985@gmail.com>
2019-05-02 17:28:54 -0400

    finished exercise 5, book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+    attr_accessor :title
+    
+    CONSONANTS = %w(and if or the a an in of).freeze
+
+    def title=(book)
+        @title = book.split(" ").map.with_index do |word, i|
+            if i == 0
+                word.capitalize 
+            elsif !CONSONANTS.include?(word)
+                word.capitalize
+            else
+                word
+            end
+        end.join(' ')
+    end
 end
+
+book1 = Book.new
+book1.title = 'the'
+puts book1.title
